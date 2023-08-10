738d81e7705c1ac22eebef61eb665004bf3f00a2
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2023-08-10 22:13:32 +0530

    loops

--- sess4.py
+++ sess4.py
@@ -1,6 +1,6 @@
 #Loops
-# while , for
-employees = ["John", "jennie", "kim", "sia", "leo", "fionna"]
+# while, for
+employees = ["John", "Jennie", "Kim", "Sia", "Leo", "Fionna"]
 name = input("Enter Employee name to search:")
 print("Length of the employees :", len(employees))
 print("Employee to Search: ", name)
@@ -16,7 +16,7 @@ if name == employees[1]:
 if name == employees[2]:
     flag = True 
 
-  # time wastage : so to reduce we use loops 
+  # time wastage: so to reduce we use loops 
 """
 
 #  LINEAR SEARCH ALGO
@@ -26,8 +26,8 @@ if name == employees[2]:
 #while loop:
 flag = False
 idx = 0
-while idx < 6: # always with while loop their is always codition 
-    print ("Matching", name , "with", employees[idx])
+while idx < 6: # always with while loop there is always condition 
+    print ("Matching", name, "with", employees[idx])
     
     if name == employees[idx]
         flag = True 
