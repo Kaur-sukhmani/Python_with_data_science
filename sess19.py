3d26f4e136b64b901a4767bbb29dc0a0b114751c
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2023-08-14 20:20:27 +0530

    Vetsapp Flask main

--- sess19.py
+++ sess19.py
@@ -105,7 +105,6 @@ def update_customer_in_db():
     return render_template('success.html', message="{} updated successfully".format(customer_data_to_update['name']))
 
 
-
 @web_app.route("/login-vet", methods=['POST'])
 def login_vet():
     vet_data = {
@@ -183,7 +182,7 @@ def fetch_all_consultations():
 
 @web_app.route("/fetch-consultation-customer-pets/<id>")
 def fetch_consultations_of_customer_pets(id):
-
+    print("Inside fetch-consultation-customer-pets ")
     db = MongoDBHelper(collection="pet")
     query = {'_id': ObjectId(id)}
     pet = db.fetch(query)[0]
@@ -225,19 +224,6 @@ def search():
     return render_template('search.html', email=session['vet_email'], name=session['vet_name'])
 
 
-@web_app.route("/add-pet/<id>")
-def add_pet(id):
-    db = MongoDBHelper(collection='customer')
-    query = {'_id': ObjectId(id)}
-    customers = db.fetch(query)
-    customer = customers[0]
-    return render_template('add-pet.html',
-                           vet_id=session['vet_id'],
-                           email=session['vet_email'],
-                           name=session['vet_name'],
-                           customer=customer
-                           )
-
 @web_app.route("/search-customer", methods = ['POST'])
 def search_customer():
     db = MongoDBHelper(collection='customer')
@@ -252,6 +238,19 @@ def search_customer():
                                )
     else:
         return render_template("error.html", message="Customer not found..")
+
+@web_app.route("/add-pet/<id>")
+def add_pet(id):
+    db = MongoDBHelper(collection='customer')
+    query = {'_id': ObjectId(id)}
+    customers = db.fetch(query)
+    customer = customers[0]
+    return render_template('add-pet.html',
+                           vet_id=session['vet_id'],
+                           email=session['vet_email'],
+                           name=session['vet_name'],
+                           customer=customer
+                           )
 
 @web_app.route("/save-pet", methods = ['POST'])
 def save_pet():
@@ -278,12 +277,15 @@ def save_pet():
     return render_template('success.html', message="{} added for customer {} successfully.."
                            .format(pet_data['name'], pet_data['customer_email']))
 
-web_app.route("/add-consultation/<id>")
+
+@web_app.route("/add-consultation/<id>")
 def add_consultation(id):
     db = MongoDBHelper(collection='pet')
+    print("in_add_consultaton")
     query = {'_id': ObjectId(id)}
     pets = db.fetch(query)
     pet = pets[0]
+    print("running add_consultaton html file")
     return render_template('add-consultation.html',
                            vet_id=session['vet_id'],
                            email=session['vet_email'],
@@ -292,40 +294,111 @@ def add_consultation(id):
                            )
 
 
-@web_app.route("/save-consultation", methods = ['POST'])
+@web_app.route("/save-consultation", methods=['POST'])
 def save_consultation():
+    print("in_save_consultaton")
     consultation_data = {
         'problem': request.form['problem'],
         'heartrate': int(request.form['heartrate']),
         'temperature': float(request.form['temperature']),
-        'gender': request.form['gender'],
+        'medicines': request.form['medicines'],
         'pet_name': request.form['pet_name'],
         'pet_id': request.form['pet_id'],
         'customer_id': request.form['customer_id'],
-        'customer_email': request.form['customer_email'],
         'vet_id': session['vet_id'],
         'createdOn': datetime.datetime.today()
     }
+    print("running save_consultaton html file")
 
     if len(consultation_data['problem']) == 0 or len(consultation_data['medicines']) == 0:
-        return render_template('error.html', message="Name and Breed cannot be Empty")
+        return render_template('error.html', message="Problem and Medicine cannot be Empty")
 
     print(consultation_data)
     db = MongoDBHelper(collection="consultation")
     db.insert(consultation_data)
 
-    return render_template('success.html', message="{} added for customer {} successfully.."
-                           .format(consultation_data['name'], consultation_data['customer_email']))
+    return render_template('success.html', message="Consultation for Pet {} added successfully..".format(consultation_data['pet_name']))
 
 
-# @web_app.route("/delete-pet/<id>")
-# def delete_pet(id):
-#     db = MongoDBHelper(collection='pet')
-#     query = {'_id': ObjectId(id), 'customer_id'}
-#     customer = db.fetch(query)[0]
-#     db.delete(query)
-#     return render_template('success.html', message="Customer {} deleted".format(customer['name']))
+@web_app.route("/delete-consultation/<id>")
+def delete_consultation(id):
+    print("Inside delete consultation pets ")
 
+    db = MongoDBHelper(collection="consultation")
+    query = {'_id': ObjectId(id)}
+    consultations = db.fetch(query)
+    print(consultations)
+    consultation = consultations[0]
+    db.delete(consultation)
+    return render_template('success.html', message="Consultation {} deleted with {} problem".format(id, consultation['problem']))
+
+@web_app.route("/update-consultation/<id>")
+def update_consultation(id):
+    db = MongoDBHelper(collection="consultation")
+    query = {'_id': ObjectId(id)}
+    consultation = db.fetch(query)[0]
+    return render_template("update-consultation.html",email=session['vet_email'],
+                           name=session['vet_name'], consultation=consultation)
+
+
+# Add this route to handle the form submission from update-consultation.html
+@web_app.route("/update-consultation-db", methods=['POST'])
+def update_consultation_db():
+    updated_consultation_data = {
+        'problem': request.form['problem'],
+        'heartrate': int(request.form['heartrate']),
+        'temperature': float(request.form['temperature']),
+        'medicines': request.form['medicines'],
+    }
+
+    consultation_id = request.form['consultation_id']  # Assuming you have an input field named consultation_id
+
+    if len(updated_consultation_data['problem']) == 0 or len(updated_consultation_data['medicines']) == 0:
+        return render_template('error.html', message="Problem and Medicine cannot be Empty")
+
+    print(updated_consultation_data)
+
+    db = MongoDBHelper(collection="consultation")
+    query = {'_id': ObjectId(consultation_id)}
+    db.update(updated_consultation_data, query)
+
+    return render_template('success.html', message="Consultation updated successfully")
+
+
+@web_app.route("/delete-pet/<id>")
+def delete_pet(id):
+    db = MongoDBHelper(collection='pet')
+    query = {'_id': ObjectId(id)}
+    pet = db.fetch(query)[0]
+    query = {'_id': ObjectId(id), 'customer_id': pet['customer_id']}
+    db.delete(query)
+    return render_template('success.html', message="Customer {} deleted".format(pet['name']))
+
+@web_app.route("/update-pet/<id>")
+def update_pet(id):
+    db = MongoDBHelper(collection='pet')
+    query = {'_id': ObjectId(id)}
+    pet = db.fetch(query)[0]
+    return render_template('update-pet.html', message="Pet with Id {} and Name {} Updated Successfully".format(id, pet['name']), email=session['vet_email'],
+                           name=session['vet_name'], pet=pet)
+
+@web_app.route("/update-pet-db", methods=['POST'])
+def update_pet_in_db():
+    pet_data_to_update = {
+        'name': request.form['name'],
+        'breed': request.form['breed'],
+        'age': int(request.form['age']),
+        'gender': request.form['gender']
+    }
+    if len(pet_data_to_update['name']) == 0 :
+        return render_template('error.html', message="Name and age cannot be Empty")
+
+    print(pet_data_to_update)
+    db = MongoDBHelper(collection='pet')
+    query = {'name': request.form['name']}
+    db.update(pet_data_to_update, query)
+
+    return render_template('success.html', message="{} updated successfully".format(pet_data_to_update['name']))
 
 @web_app.route("/logout")
 def logout():
