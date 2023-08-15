df8d15a8c9f36a42a2715a7af682a725a63c3b17
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2023-08-15 23:19:07 +0530

    web scrapping ipl-2021

---
+++ sess25A.py
@@ -0,0 +1,51 @@
+# ipl data 2021
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.espncricinfo.com/series/ipl-2021-1249214/points-table-standings"
+response = requests.get(url)
+print(response.text)
+
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup.prettify())
+
+teams = soup.findAll("span", class_="ds-text-tight-s ds-font-bold ds-uppercase ds-text-left ds-text-typo")
+wins = soup.findAll("td", class_="ds-w-0 ds-whitespace-nowrap ds-min-w-max")
+
+# to create a table
+data = []
+for idx in range(8):
+    team_data = []
+    data.append(team_data)
+print(data)
+
+idx = 0
+for team in teams:
+    title = team.text.strip()
+    data[idx].append(title)
+    idx += 1
+print(data)
+
+team_idx = 0
+for win in wins:
+    num = win.text.strip()
+    print(num)
+
+    data[team_idx].append(num)
+    idx += 1
+
+    if idx % 8 == 0:
+        print("wow..")
+        team_idx += 1
+
+print("data finally...", data)
+
+file = open("ipl-data-2021.csv", "a")
+header = "TEAMS, M, W, L, T, N/R, NRR, For, Against"
+file.write(header)
+
+for info in data:
+    csv = "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n".format(info[0], info[1], info[2], info[3], info[4], info[4], info[5], info[6], info[7], info[8])
+    file.write(csv)
+    print(csv)
+print("Check the file....")
