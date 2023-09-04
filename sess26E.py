380a9de154e41db786face6a01a56c23bd94e6c7
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2023-09-04 18:42:52 +0530

    list of strings

---
+++ sess26E.py
@@ -0,0 +1,15 @@
+# file = open("ipl-data-2022.csv", "r")
+#
+# # List of strings
+# lines = file.readlines()
+#
+# for line in lines:
+#     print(line)
+"""upper or down """
+
+with open("ipl-data-2022.csv", "r") as file:
+    lines = file.readlines()
+
+    for line in lines:
+        print(line)
+
